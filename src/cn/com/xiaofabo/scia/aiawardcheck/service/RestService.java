010867f9228172c2d0fa3eb5ae4d65fae196f747
Ben Chen <jinerben@gmail.com>
2019-07-02 00:00:44 +0200

    Trying to save uploaded file to correct file. Not successful

--- src/cn/com/xiaofabo/scia/aiawardcheck/service/RestService.java
+++ src/cn/com/xiaofabo/scia/aiawardcheck/service/RestService.java
@@ -1,6 +1,10 @@
 package cn.com.xiaofabo.scia.aiawardcheck.service;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -18,16 +22,39 @@ public class RestService {
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String postTest(File inputFile) {
+	public String postTest(InputStream uploadedInputStream) {
 		try {
-			System.out.println(inputFile.length());
-			AwardReader arbReader = new AwardReader();
-			Award ab = arbReader.buildAward(inputFile);
-			AwardWriter arbWriter = new AwardWriter("out.docx");
-			arbWriter.generateAwardDoc(ab);
+			writeToFile(uploadedInputStream, "./input.docx");
+			File inputFile = new File("./input.docx");
+			System.out.println(inputFile.getAbsolutePath());
+			// AwardReader arbReader = new AwardReader();
+			// Award ab = arbReader.buildAward(inputFile);
+			// AwardWriter arbWriter = new AwardWriter("out.docx");
+			// arbWriter.generateAwardDoc(ab);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return inputFile.length()+"";
+		return "Successful!";
+	}
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 }
